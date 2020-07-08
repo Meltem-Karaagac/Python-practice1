7a441ece2c5277ffdef9cd831d60c91fd18ee819
meltem <meltemkaraagac19@gmail.com>
2020-07-08 02:06:35 -0400

    second time update

---
+++ Asigmment_2.py
@@ -0,0 +1,8 @@
+cigaretteAdd = input("Do you addict to Cigarette?").lower()
+Old = input("Are you older than 75?").lower();
+Chronic =input("Do you have cronical illness?").lower();
+weakImmune = input("Do you have week immunity?").lower();
+if ((cigaretteAdd == "yes" and Old == "yes") or weakImmune == "yes" or Chronic == "yes" or Chronic == "y"):
+        print("You are in risky group")
+else : 
+    print("You are not in risky group");
