6489b1929e288853a473212b605d620f751d5d3c
meltem <meltemkaraagac19@gmail.com>
2020-07-09 23:49:42 -0400

    proje_push

---
+++ proje_6.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+a = set('philadelphia')
+print (a)
+
+
+# In[10]:
+
+
+a = set ('07/04/2020')
+print(a)
+
+
+# In[9]:
+
+
+a = {'07/04/2020'}
+print(a)
+
+
+# In[ ]:
+
+
+{'1','2','3','4','5'}
+
+
+# In[22]:
+
+
+usa = set('washington')
+zea = set('wellington')
+print (usa)
+print (zea)
+print(usa-zea)
+print(usa.difference (zea))
+print(usa.intersection(zea))
+print(usa|(zea))
+print(usa&(zea))
+
+
+# In[29]:
+
+
+minced = true 
+grocer = true
+bread = true
+lettuce = true
+pepper = true
+hamburger = (minced and grocer and bread) and (lettuce or pepper)
+if hamburger :
+    print
+
+
+# In[38]:
+
+
+x = 6
+y = 9
+print (x == y)
+print (x != y)
+print (x < y)
+
+
+# In[ ]:
+
+
+
+
